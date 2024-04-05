b984150c1e59c21fbf9d896ee5c5825bb5f5b42b
Danendra Satriyo Buwono <xxdanen@gmail.com>
2024-04-05 17:46:09 +0700

    Melanjutkan tampilan utama, form, dan radio button aplikasi, serta membuat tombol untuk membuat invoice dan tombol bagikan

--- app/src/main/java/org/d3if3007/assessment1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3007/assessment1/ui/screen/MainScreen.kt
@@ -1,19 +1,26 @@
 package org.d3if3007.assessment1.ui.screen
 
+import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -25,16 +32,21 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.d3if3007.assessment1.R
@@ -69,17 +81,26 @@ fun ScreenContent(modifier: Modifier) {
     var jumlahError by rememberSaveable { mutableStateOf(false) }
 
     val radioOptions = listOf(
-        stringResource(id = R.string.burger),
-        stringResource(id = R.string.hot_dog),
-        stringResource(id = R.string.fried_chiken)
+        stringResource(id = R.string.dine_in),
+        stringResource(id = R.string.take_away),
     )
-    var menu by rememberSaveable {
+    var orderOption by rememberSaveable {
         mutableStateOf(radioOptions[0])
     }
+
+    var totalHarga by rememberSaveable {
+        mutableFloatStateOf(0f)
+    }
+    var kategori by rememberSaveable {
+        mutableIntStateOf(0)
+    }
+
+    val context = LocalContext.current
 
     Column(
         modifier = modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(12.dp),
         horizontalAlignment = Alignment.CenterHorizontally
@@ -99,7 +120,8 @@ fun ScreenContent(modifier: Modifier) {
             singleLine = true,
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Ascii,
-                imeAction = ImeAction.Done
+                imeAction = ImeAction.Done,
+                capitalization = KeyboardCapitalization.Characters
             ),
             modifier = Modifier.fillMaxWidth()
         )
@@ -121,17 +143,18 @@ fun ScreenContent(modifier: Modifier) {
         Row(
             modifier = Modifier
                 .padding(top = 6.dp)
-                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
-
+                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp)),
+            verticalAlignment = Alignment.CenterVertically
         )
         {
             radioOptions.forEach { text ->
-                MenuOption(label = text,
-                    isSelected = menu == text,
+                OrderOption(
+                    label = text,
+                    isSelected = orderOption == text,
                     modifier = Modifier
                         .selectable(
-                            selected = menu == text,
-                            onClick = { menu = text },
+                            selected = orderOption == text,
+                            onClick = { orderOption = text },
                             role = Role.RadioButton
                         )
                         .weight(1f)
@@ -139,11 +162,47 @@ fun ScreenContent(modifier: Modifier) {
                 )
             }
         }
+        Button(
+            onClick = {
+                namaError = (nama == "")
+                jumlahError = (jumlah == "" || jumlah == "0")
+                if (namaError || jumlahError) return@Button
+
+                totalHarga = totalHarga(jumlah.toFloat())
+                kategori = getKategori(orderOption==radioOptions[0])
+            },
+            modifier = Modifier.padding(top = 8.dp),
+            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+        ) {
+            Text(text = stringResource(id = R.string.pesan))
+        }
+        if (totalHarga != 0f) {
+            Divider(
+                modifier = Modifier.padding(vertical = 8.dp),
+                thickness = 1.dp
+            )
+            Text(
+                text = stringResource(id = R.string.invoice, nama, jumlah, totalHarga, context.getString(kategori)),
+                style = MaterialTheme.typography.titleLarge,
+                textAlign = TextAlign.Justify
+            )
+            Button(onClick = {
+                shareData(
+                    context = context,
+                    message = context.getString(R.string.bagikan_template, jumlah, totalHarga, context.getString(kategori))
+                )
+            },
+                modifier = Modifier.padding(top = 8.dp),
+                contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+            ) {
+                Text(text = stringResource(id = R.string.bagikan))
+            }
+        }
     }
 }
 
 @Composable
-fun MenuOption(label: String, isSelected: Boolean, modifier: Modifier) {
+fun OrderOption(label: String, isSelected: Boolean, modifier: Modifier) {
     Row(
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically
@@ -152,7 +211,7 @@ fun MenuOption(label: String, isSelected: Boolean, modifier: Modifier) {
         Text(
             text = label,
             style = MaterialTheme.typography.bodyLarge,
-            modifier = Modifier.padding(start = 8.dp)
+            modifier = Modifier.padding(start = 8.dp),
         )
     }
 }
@@ -170,6 +229,33 @@ fun ErrorHint(isError: Boolean) {
         Text(text = stringResource(id = R.string.input_invalid))
     }
 }
+
+private fun totalHarga(jumlah: Float): Float {
+    return jumlah * 20000
+}
+
+private  fun getKategori(order:Boolean):Int{
+    return if(order){
+        R.string.dine_in
+    }else{
+        R.string.take_away
+    }
+}
+
+private fun shareData(
+    context: Context,
+    message: String,
+
+    ){
+    val shareIntent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_TEXT, message)
+    }
+    if(shareIntent.resolveActivity(context.packageManager) != null){
+        context.startActivity(shareIntent)
+    }
+}
+
 
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
