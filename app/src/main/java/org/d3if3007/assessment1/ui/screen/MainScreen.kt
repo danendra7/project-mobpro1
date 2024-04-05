09e64922e70a6867281e07d6ffe3489e0b0e33a5
Danendra Satriyo Buwono <xxdanen@gmail.com>
2024-04-05 18:26:34 +0700

    Membuat navigasi menggunakan menu, dengan tombol up

--- app/src/main/java/org/d3if3007/assessment1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3007/assessment1/ui/screen/MainScreen.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
 import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -19,10 +18,12 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -49,12 +50,15 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import org.d3if3007.assessment1.R
+import org.d3if3007.assessment1.navigation.Screen
 import org.d3if3007.assessment1.ui.theme.Assessment1Theme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen() {
+fun MainScreen(navController: NavHostController) {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -64,7 +68,16 @@ fun MainScreen() {
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary
-                )
+                ),
+                actions = {
+                    IconButton(onClick = {navController.navigate(Screen.About.route)}) {
+                        Icon(
+                            imageVector = Icons.Outlined.Info,
+                            contentDescription = stringResource(id = R.string.tentang_aplikasi),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
             )
         }
     ) { padding ->
@@ -169,7 +182,7 @@ fun ScreenContent(modifier: Modifier) {
                 if (namaError || jumlahError) return@Button
 
                 totalHarga = totalHarga(jumlah.toFloat())
-                kategori = getKategori(orderOption==radioOptions[0])
+                kategori = getKategori(orderOption == radioOptions[0])
             },
             modifier = Modifier.padding(top = 8.dp),
             contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
@@ -182,16 +195,28 @@ fun ScreenContent(modifier: Modifier) {
                 thickness = 1.dp
             )
             Text(
-                text = stringResource(id = R.string.invoice, nama, jumlah, totalHarga, context.getString(kategori)),
+                text = stringResource(
+                    id = R.string.invoice,
+                    nama,
+                    jumlah,
+                    totalHarga,
+                    context.getString(kategori)
+                ),
                 style = MaterialTheme.typography.titleLarge,
                 textAlign = TextAlign.Justify
             )
-            Button(onClick = {
-                shareData(
-                    context = context,
-                    message = context.getString(R.string.bagikan_template, jumlah, totalHarga, context.getString(kategori))
-                )
-            },
+            Button(
+                onClick = {
+                    shareData(
+                        context = context,
+                        message = context.getString(
+                            R.string.bagikan_template,
+                            jumlah,
+                            totalHarga,
+                            context.getString(kategori)
+                        )
+                    )
+                },
                 modifier = Modifier.padding(top = 8.dp),
                 contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
             ) {
@@ -234,10 +259,10 @@ private fun totalHarga(jumlah: Float): Float {
     return jumlah * 20000
 }
 
-private  fun getKategori(order:Boolean):Int{
-    return if(order){
+private fun getKategori(order: Boolean): Int {
+    return if (order) {
         R.string.dine_in
-    }else{
+    } else {
         R.string.take_away
     }
 }
@@ -246,12 +271,12 @@ private fun shareData(
     context: Context,
     message: String,
 
-    ){
+    ) {
     val shareIntent = Intent(Intent.ACTION_SEND).apply {
         type = "text/plain"
         putExtra(Intent.EXTRA_TEXT, message)
     }
-    if(shareIntent.resolveActivity(context.packageManager) != null){
+    if (shareIntent.resolveActivity(context.packageManager) != null) {
         context.startActivity(shareIntent)
     }
 }
@@ -262,6 +287,6 @@ private fun shareData(
 @Composable
 fun AboutScreenPreview() {
     Assessment1Theme {
-        MainScreen()
+        MainScreen(rememberNavController()  )
     }
 }
